bb2fffb393962854b3cd1df6364411775cf8a98b
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2023-06-23 22:39:20 -0400

    Added Regex validation class, started beginning of program

--- HillCavernFair/Program.cs
+++ HillCavernFair/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿/*Welcome to Hill Cavern Fair, an adventure game. 
+ * Coded by Brandi Hornbuckle (https://github.com/OrenjiId3s and https://linkedin.com/in/bhornbuckle)
+ * and story by Calliope Woods (https://www.calliopewoods.com).*/
+
+using Spectre.Console;
+
+namespace HillCavernFair;
+
+public class Program
+{
+    private static string? playerName;
+    private static string? playerEmail;
+
+    public static string? PlayerName { get => playerName; set => playerName = value; }
+    public static string? PlayerEmail { get => playerEmail; set => playerEmail = value; }
+
+    public static void Main(string[] args)
+    {
+        Console.Title = "Hill Cavern Fair";
+
+        AnsiConsole.Write(
+            new FigletText("HILL CAVERN FAIR")
+            .Centered()
+            .Color(Color.DarkGoldenrod));
+
+        Console.WriteLine();
+
+        AnsiConsole.Markup("[mediumpurple1]Code by Brandi Hornbuckle (https://github.com/OrenjiId3s)[/]");
+
+        Console.WriteLine();
+
+        AnsiConsole.Markup("[hotpink3_1]Story by Calliope Woods (https://www.calliopewoods.com)[/]");
+
+        Console.WriteLine();
+
+        AnsiConsole.Markup("[darkgoldenrod]Welcome to Hill Cavern Fair. This is best viewed in full screen mode.[/]");
+
+        Console.WriteLine();
+
+        Console.ReadKey();
+    }
+}
---
+++ HillCavernFair/RegexEmail.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HillCavernFair
+{
+    public static class RegexEmail
+	{
+        public static bool Validate(string emailAddress)
+        {
+            var regex = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+            bool isValid = Regex.IsMatch(emailAddress, regex, RegexOptions.IgnoreCase);
+            return isValid;
+        }
+    }
+}
+
