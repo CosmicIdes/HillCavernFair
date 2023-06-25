b29d5ad2c78ce43ad910f731d3efd49b9005c160
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2023-06-25 11:42:13 -0400

    Beginnings of database setup using SQLite

---
+++ HillCavernFairData/HillCavernFairContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HillCavernFair.Domain;
+
+namespace HillCavernFair.Data;
+
+public class HillCavernFairContext: DbContext
+{
+    public DbSet<Paragraph> Paragraphs { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(
+             "Server = :memory:; Database = HillCavernFairAppData; Trusted_Connection = True; ");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Paragraph>()
+            .HasKey(s => new { s.Id });
+
+        // base.OnModelCreating(modelBuilder);
+    }
+}
+
